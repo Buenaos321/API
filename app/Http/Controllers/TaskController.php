dedacc7238f9fb85f5710d583fff96a5810a0c5c
Buenaos321 <leonardobuenanos321@gmail.com>
2021-09-09 01:21:50 -0500

    Se crearon las rutas de la api y sus metodos, fin api

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return \response($tasks);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=>'required',
+            'description'=>'required|max:255',
+            'status'=>'required'
+        ]);
+
+        $tasks = Task::create($request->all());
+        return \response($tasks);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $tasks = Task::findOrFail($id);
+        return \response($tasks);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $tasks = Task::findOrFail($id)->update($request->all());
+        return \response('Tarea Actualizada');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Task::destroy($id);
+        return \response('La tarea con el id: ${id} ha sido eliminada');
+    }
+}
